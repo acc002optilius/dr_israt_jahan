0d2b130d18feccc7ebe3a8dd9304fd02f82f8c3f
Md. Sirajul Islam.dev <acc004@optilius.com>
2025-04-13 11:20:25 +0600

    up

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente ab tenetur architecto eos unde dolores amet debitis expedita, inventore saepe! Aliquid tempora expedita earum asperiores veritatis cum, saepe ut nostrum?
+    </div>
   )
 }
 
